85c72f6da7643dbe2082f7878f4408e1013be80b
Ashwani Saha <ashwani.saha@Indegene.com>
2020-01-20 11:36:21 +0530

    deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Template from './_components/template/template';
-import Editor from './_components/editor/editor';
 function App() {
   return (
     <>
       <Router>
         <>
           <Route exact path="/" component={Template} />
-          <Route exact path="/editor" component={Editor} />
         </>
       </Router>
     </>
